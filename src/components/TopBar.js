65583fffe4271ee58f2cebb19b5d55de52d592f4
Luis Palma <luisarnaldopalma@gmail.com>
2022-10-29 18:04:33 +0200

    Pepe

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import foto from '../assets/images/castorLogo.webp';
+import foto from '../assets/images/Pepe-argento.webp';
 
 function TopBar(){
     return(
@@ -14,12 +14,32 @@ function TopBar(){
 
 					{/*<!-- Topbar Navbar -->*/}
 					<ul className="navbar-nav ml-auto">
+
+						{/*<!-- Nav Item - Alerts -->*/}
+						<li className="nav-item dropdown no-arrow mx-1">
+							<a className="nav-link dropdown-toggle" href="/" id="alertsDropdown">
+								<i className="fas fa-bell fa-fw"></i>
+								{/*<!-- Counter - Alerts -->*/}
+								<span className="badge badge-danger badge-counter">1+</span>
+							</a>
+						</li>
+
+						{/*<!-- Nav Item - Messages -->*/}
+						<li className="nav-item dropdown no-arrow mx-1">
+							<a className="nav-link dropdown-toggle" href="/" id="messagesDropdown">
+								<i className="fas fa-envelope fa-fw"></i>
+								{/*<!-- Counter - Messages -->*/}
+								<span className="badge badge-danger badge-counter">3</span>
+							</a>
+						</li>
+
 						<div className="topbar-divider d-none d-sm-block"></div>
 
 						{/*<!-- Nav Item - User Information -->*/}
 						<li className="nav-item dropdown no-arrow">
 							<a className="nav-link dropdown-toggle" href="/" id="userDropdown">
-								<img className="img-profile rounded-circle" src={foto} alt="Logo Castor" width="60"/>
+								<span className="mr-2 d-none d-lg-inline text-gray-600 small">Pepe Argento</span>
+								<img className="img-profile rounded-circle" src={foto} alt="Pepe Argento" width="60"/>
 							</a>
 						</li>
 
